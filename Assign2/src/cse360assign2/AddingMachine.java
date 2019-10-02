3f58cfbb5f2205760ac4a7a76c67fa55efc95e36
Trutinn <pmott@asu.edu>
2019-10-01 20:32:04 -0700

    Committed for the first time

---
+++ Assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,40 @@
+/*
+Preston Mott, 70641, Assignment 2 
+*/
+package cse360assign2;
+
+
+public class AddingMachine {
+
+	private int total;
+	
+	//constructor to set the total to be equal to 0
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	//returns the current total
+	public int getTotal () {
+		return 0;
+	}
+	
+	//add the parameter to the current total
+	public void add (int value) {
+		
+	}
+	
+	//subtract the parameter to the current total
+	public void subtract (int value) {
+		
+	}
+		
+	//returns all past operations performed
+	public String toString () {
+		return "";
+	}
+
+	//clears the history
+	public void clear() {
+	
+	}
+}
